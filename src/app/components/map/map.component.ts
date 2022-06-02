c46787d3459cc59d09b85657e18a08af09a9da46
Jakub Lasek <jakublasek41@gmail.com>
2022-06-02 09:23:41 +0200

    Add base map initialization using leaflet

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit {
+  map!: L.Map;
+  private startingPosition: L.LatLngExpression = [50.090683, 19.974544];
 
-  constructor() { }
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: this.startingPosition,
+      zoom: 15,
+      zoomControl: false,
+    });
 
-  ngOnInit(): void {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 10,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.map);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.initMap();
+  }
 }
