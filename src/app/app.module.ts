0586dbc217475124f81b8a9f7da2ed9b1957262e
Jakub Lasek <jakublasek41@gmail.com>
2022-06-03 20:36:21 +0200

    Add Backdrop + move CoordinatesForm to map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { PopupComponent } from './components/popup/popup.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { ErrorMessageComponent } from './components/error-message/error-message.component';
 import { PolygonEditButtonComponent } from './components/polygon-edit-button/polygon-edit-button.component';
+import { BackdropComponent } from './components/backdrop/backdrop.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { PolygonEditButtonComponent } from './components/polygon-edit-button/pol
     MainLayoutComponent,
     ErrorMessageComponent,
     PolygonEditButtonComponent,
+    BackdropComponent,
   ],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
