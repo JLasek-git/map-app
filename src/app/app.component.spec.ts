379650bd5ed8a4077ccf487c6a2ba718ac9b27d6
Jakub Lasek <jakublasek41@gmail.com>
2022-06-03 15:49:56 +0200

    Project name change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'zadanie-rekrutacyjne'`, () => {
+  it(`should have as title 'map-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('zadanie-rekrutacyjne');
+    expect(app.title).toEqual('map-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('zadanie-rekrutacyjne app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'map-app app is running!'
+    );
   });
 });
